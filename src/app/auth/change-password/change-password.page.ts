b3b109be10558a669db86687394bb91482853810
Cesar Delgado <cdelgado@globaldata.com.ar>
2021-06-11 07:10:45 -0300

    Diseño e interacción de controles para habilitar y deshabilitar contraseña.

--- src/app/auth/change-password/change-password.page.ts
+++ src/app/auth/change-password/change-password.page.ts
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-password.page.scss'],
 })
 export class ChangePasswordPage implements OnInit{
+  password = {
+    current: 'password',
+    new: 'password',
+    repeat: 'password'
+  };
   constructor(){ }
   ngOnInit(){}
+  change(item)
+  {
+    this.password[item] = (this.password[item] === 'password') ? 'text': 'password';
+  }
 }
